0594f00b750d8daaa698499355ef762776f829e0
Tsunoda-s-FT <tsunoda@friendlytech.jp>
2025-03-06 08:30:05 +0900

    Remove SWC minification and adjust layout component to fix header rendering

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function Layout({ children, header }) {
   return (
     <div className="h-full flex flex-col bg-slate-50 relative">
-      {header}
+      
       <main className="h-full pt-14">
         {children}
       </main>
